679ad956639fbdb0cb2302f7b2431192f9584f0c
petrova-maria-a <125276344+petrova-maria-a@users.noreply.github.com>
2025-11-17 00:51:43 +0300

    добавила \n

--- 2 сумма чисел.py
+++ 2 сумма чисел.py
@@ -1,4 +1,4 @@
-num1 = int(input('Введите первое число: '))
-num2 = int(input('Введите второе число: '))
+num1 = int(input('Введите первое число: \n'))
+num2 = int(input('Введите второе число: \n'))
 print(num1 + num2)
 
