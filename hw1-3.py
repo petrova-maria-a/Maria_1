b0bc5b6759fa57cd5370da10719da381b3c3a39b
petrova-maria-a <125276344+petrova-maria-a@users.noreply.github.com>
2025-11-20 17:36:55 +0300

    Криво работает этот кусок: else:             num = num // 5

--- hw1-3.py
+++ hw1-3.py
@@ -5,7 +5,7 @@ while input() != "стоп":
     if num / 100 >= 1:
         if num % 3 == 0:
             num = num ** 2
-        elif:
+        else:
             num = num // 5
     if num / 100 < 1:
         num = (num * 8) % 6
