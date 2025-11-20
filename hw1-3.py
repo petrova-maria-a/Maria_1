3eb8432d27beaa9fa0f55ba0dacb451f110a60cd
petrova-maria-a <125276344+petrova-maria-a@users.noreply.github.com>
2025-11-20 17:30:54 +0300

    переписала, но пока с косяком

--- hw1-3.py
+++ hw1-3.py
@@ -1,11 +1,12 @@
-print('Введите трехзначное число (номер машины)')
-car = int(input())
-print('Введите однозначное или двузначное число')
-people = int(input())
-if car % 3 == 0:
-    car = car**2
-else:
-    car = car // 5
-people = (people*8)%6
-print(car)
-print(people)
+print("Введите 'стоп' для завершения или что угодно для продолжения")
+while input() != "стоп":
+    print('Введите число от 1 до 999')
+    num = int(input())
+    if num / 100 >= 1:
+        if num % 3 == 0:
+            num = num ** 2
+        elif:
+            num = num // 5
+    if num / 100 < 1:
+        num = (num * 8) % 6
+    print(num)
